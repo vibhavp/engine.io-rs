643b191d149ef12f8214e4f6ca1d44c0493a9d44
Vibhav Pant <Vibhav Pant>
2016-06-30 23:26:14 +0530

    Add is_binary, complete encode_payload

--- src/packet.rs
+++ src/packet.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 use hyper::server::request::Request;
 use rand::os::OsRng;
 use rand::Rng;
+use rustc_serialize::base64::{ToBase64, Config, CharacterSet, Newline};
 
 #[derive(Copy, Clone)]
 pub enum ID {
@@ -49,6 +50,15 @@ impl Packet {
         })
     }
 
+    fn is_binary(&self) -> bool {
+        for c in &self.data {
+            if *c < 'a' as u8 || *c > 'Z' as u8 {
+                return true
+            }
+        }
+        false
+    }
+
     pub fn encode(&self) -> Vec<u8> {
         let mut vec = Vec::new();
 
@@ -66,7 +76,7 @@ impl Packet {
             v.push(*b)
         }
     }
-    
+
     fn open_json(sid: String, ping_timeout: Duration) -> String {
         format!("{{\"sid\": {}, \"upgrades\": {}, \"pingTimeout\": {}}}", sid,
                 "websocket", ping_timeout.as_secs() * 1000)
@@ -77,9 +87,57 @@ impl Packet {
     }
 }
 
-pub fn payload(packets: &[Packet], j: i32, b64: bool) -> Vec<u8> {
+/// Encode a series of packets, for polling and flashsocket transports.
+///
+/// # Arguments:
+///
+/// * `packets`: A vector of packets to be encoded together.
+/// * `jsonp_index`: JSONP response index, if any.
+/// * `b64`: true if binary packets are to be encoded in base64.
+/// * `xhr2`: true if the packets are to be encoded with XHR2 support.
+pub fn encode_payload(packets: Vec<Packet>, jsonp_index: Option<i32>, b64: bool, xhr2: bool) -> Vec<u8> {
     let mut data = Vec::new();
+    let mut jsonp = false;
+
+    jsonp_index.map(|index| {
+        for c in format!("__eio[{}](",index).as_bytes() {
+            data.push(*c);
+        }
+        jsonp = true;
+    });
+
     for packet in packets {
-        
+        if b64 {
+            let base64_data = packet.data.to_base64(Config{
+                char_set: CharacterSet::UrlSafe,
+                newline: Newline::LF,
+                pad: true,
+                line_length: None,
+            });
+            for c in (base64_data.len() + 1).to_string().chars() {
+                data.push(c.to_digit(10).unwrap() as u8);
+            }
+            data.push(':' as u8);
+            data.push('b' as u8);
+            data.extend_from_slice(base64_data.as_bytes());
+        } else {
+            if xhr2 {
+                data.push(packet.is_binary() as u8);
+                data.push(0);
+                data.push(255);
+            }
+
+            for c in packet.data.len().to_string().chars() {
+                data.push(c.to_digit(10).unwrap() as u8);
+            }
+            data.push(':' as u8);
+            data.extend_from_slice(packet.encode().as_slice());
+        }
     }
+
+    if jsonp {
+        data.push(')' as u8);
+    }
+
+    data
 }
