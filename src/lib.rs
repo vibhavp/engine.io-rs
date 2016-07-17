b2597dac4ef06720185bc9c77e559c50ac6c0eab
Vibhav Pant <vibhavp@gmail.com>
2016-07-17 17:54:19 +0530

    Add more crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
-#[macro_use]
-extern crate iron;
+#[macro_use] extern crate iron;
 extern crate rand;
 extern crate url;
 extern crate rustc_serialize as serialize;
 extern crate crypto;
 extern crate modifier;
 extern crate cookie;
+extern crate time;
+extern crate hyper;
+#[macro_use] extern crate log;
 
 pub mod packet;
 pub mod server;
 pub mod socket;
+pub mod config;
 
 #[cfg(test)]
 mod tests {
